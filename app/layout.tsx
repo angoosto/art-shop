4e328932f90a5f7fc15ece2c020fa946e3cbfc2f
jumbonutpack <152092213+jumbonutpack@users.noreply.github.com>
2023-11-27 20:16:58 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bethany Loveday Art"
+  title: "Bethany Jefferson-Loveday Art"
 };
 
 export default function RootLayout({
